0c53343a6d654763de33643a5dc3d711c5b64290
mironello <mironelllo@gmail.com>
2023-12-14 12:34:35 +0200

    add public String roundValue(int checkBox, float value) method to round currencyValue

--- src/main/java/org/example/RoundValues.java
+++ src/main/java/org/example/RoundValues.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import java.text.DecimalFormat;
+
 public class RoundValues {
-    float val = 0.0F;
-     public float roundValue(float value){
-         return val;
+/*  use public String roundValue(int checkBox, float value) to get rounded value**/
+
+    public RoundValues() {
+    }
+
+    public String roundValue(int checkBox, float value){
+         switch (checkBox){
+             case (1):
+                 return  formatOutputData(value, "#.#" );
+             case (2) :
+                 return   formatOutputData(value, "#.##");
+             case (3):
+                 return   formatOutputData(value, "#.###");
+             case (4):
+                 return   formatOutputData(value, "#.####");
+         }
+        return "Invalid command";
+     }
+
+     private String formatOutputData(float value, String pattern){
+         DecimalFormat decimalFormat = new DecimalFormat(pattern);
+         return decimalFormat.format(value);
      }
 }
